e7702d15e8432272028d801ac4490ff81210321a
Monicaasensio <Monicaasensio@yahoo.com>
2023-10-12 16:33:49 -0400

    added contact.js file

---
+++ contact.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('select');
+var instances = M.FormSelect.init(elems, options);
+});
+
+M.AutoInit();
+
